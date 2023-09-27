0de5be43394d360ccf364b48888da04d8bb0a4dd
Brocal82 <antonioruizbrocal@gmail.com>
2023-09-27 17:51:08 +0200

    Profile Page Component created
    If applied this commit will introduce a profile page component that manages user data. This Angular component fetches user information from local storage and provides features for updating and deleting user accounts. It utilizes MatSnackBar for notifications and the Router for navigation, enabling users to seamlessly edit their profiles or delete their accounts.

---
+++ src/app/profile-page/profile-page.component.ts
@@ -0,0 +1,73 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service'
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
+type User = { _id?: string, Username?: string, Password?: string, Email?: string, FavoriteMovies?: [] }
+
+@Component({
+  selector: 'app-profile-page',
+  templateUrl: './profile-page.component.html',
+  styleUrls: ['./profile-page.component.scss']
+})
+export class ProfilePageComponent implements OnInit {
+  user: User = {};
+
+  @Input() userData = { Username: '', Password: '', Email: '' };
+  
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public snackBar: MatSnackBar,
+    public router: Router
+  ) { }
+
+  ngOnInit(): void {
+    const user = this.getUser();
+
+    if (!user._id) {
+      this.router.navigate(['welcome']);
+      return;
+    }
+
+    this.user = user;
+    this.userData = {
+      Username: user.Username || "",
+      Email: user.Email || "",
+      Password: ""
+    }
+  }
+
+  getUser(): User {
+    return JSON.parse(localStorage.getItem('user') || '{}');
+  }
+
+  updateUser(): void {
+    this.fetchApiData.editUser(this.userData).subscribe((response) => {
+      console.log(response)
+      localStorage.setItem('user', JSON.stringify(response))
+      this.user = response;
+      this.snackBar.open('user updated!', 'OK', {
+        duration: 2000
+      })
+    })
+  }
+
+  deleteUser(): void {
+    if (confirm('are you sure?')) {
+      this.router.navigate(['welcome']).then(() => {
+        this.snackBar.open(
+          'You have successfully deleted your account',
+          'OK',
+          {
+            duration: 2000,
+          }
+        );
+      });
+      this.fetchApiData.deleteUser().subscribe((result) => {
+        console.log(result);
+        localStorage.clear();
+      });
+    }
+  }
+
+}
